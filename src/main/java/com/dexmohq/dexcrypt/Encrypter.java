9a778cfe68d57b2410cc59dca425cdddbc5e37e7
Henrik Drefs <henrik.drefs@outlook.de>
2018-01-17 21:48:20 +0100

    zip encryption of single files

--- src/main/java/com/dexmohq/dexcrypt/Encrypter.java
+++ src/main/java/com/dexmohq/dexcrypt/Encrypter.java
@@ -15,16 +15,15 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.*;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
+import java.util.zip.*;
 
 public class Encrypter {
 
+    public static final int EOF = -1;
+
     private static byte[] randomIv(int size) throws NoSuchAlgorithmException {
-        final byte[] iv = new byte[size];
         final SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+        final byte[] iv = new byte[size];
         random.nextBytes(iv);
         return iv;
     }
@@ -160,14 +159,72 @@ public class Encrypter {
     private static final String ENCRYPTED_FILE_EXTENSION = ".dxp";
 
     public static void main(String[] args) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
-        String fileName = "D:\\dexcrypttest\\files";
+        String fileName = "D:\\dexcrypttest\\files\\video.mp4.secret";
 
-        zipFolder(fileName);
+        decryptZipped(fileName, "supersecret");
 
 //        final String password = "supersecret";
 //        encryptFile(fileName, password);
 //        decryptFile(fileName + ".dxp", password);
 
+    }
+
+    private static void encryptZipped(String fileName, String password) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IOException {
+        final Cipher cipher = newCipher();
+
+        cipher.init(Cipher.ENCRYPT_MODE, getKey(password));
+
+        final File file = new File(fileName);
+        try (InputStream in = new BufferedInputStream(new FileInputStream(file))) {
+            final byte[] buffer = new byte[BUFFER_SIZE];
+            final ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream(BUFFER_SIZE);
+            final ZipOutputStream zipped = new ZipOutputStream(byteBuffer);
+            zipped.putNextEntry(new ZipEntry(file.getName()));
+            final CipherOutputStream out = new CipherOutputStream(new BufferedOutputStream(new FileOutputStream(new File(fileName + ".secret"))), cipher);
+            int read;
+            while ((read = in.read(buffer)) != EOF) {
+                zipped.write(buffer, 0, read);
+                final byte[] zippedBytes = byteBuffer.toByteArray();
+                out.write(zippedBytes);
+                byteBuffer.reset();
+            }
+            zipped.closeEntry();
+            // calling zipped.close() is obsolete, because the underlying output is in-memory
+            out.close();
+        }
+    }
+
+    private static void decryptZipped(String fileName, String password) throws FileNotFoundException, InvalidKeyException {
+        final Cipher cipher = newCipher();
+        cipher.init(Cipher.DECRYPT_MODE, getKey(password));
+        try (ZipInputStream in = new ZipInputStream(new CipherInputStream(new BufferedInputStream(new FileInputStream(new File(fileName))), cipher))) {
+
+            final OutputStream out = new BufferedOutputStream(new FileOutputStream(new File(fileName.substring(0, fileName.length() - ".secret".length()))));
+
+            final byte[] buffer = new byte[BUFFER_SIZE];
+
+            int read;
+            in.getNextEntry();
+            while ((read = in.read(buffer)) > 0) {
+                out.write(buffer, 0, read);
+            }
+            out.close();
+
+        } catch (EOFException e) {
+            // ignore
+        } catch (FileNotFoundException e) {
+            throw e;//rethrow to avoid it getting caught with the IOException
+        } catch (IOException e) {
+            throw new RuntimeException(e);//todo
+        }
+    }
+
+    private static Cipher newCipher() {
+        try {
+            return Cipher.getInstance("AES/ECB/PKCS5Padding");
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new InternalError(e);
+        }
     }
 
 
